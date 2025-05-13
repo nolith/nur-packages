c8b335ae1bcaa79dc60b742046db97f3d3ff7523
Alessio Caiazza <code.git@caiazza.info>
2025-05-13 18:03:53 +0200

    fix and format

--- flake.nix
+++ flake.nix
@@ -1,14 +1,36 @@
 {
   description = "My personal NUR repository";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  outputs = { self, nixpkgs }:
-    let
-      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
-    in
-    {
-      legacyPackages = forAllSystems (system: import ./default.nix {
-        pkgs = import nixpkgs { inherit system; };
-      });
-      packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+  outputs = inputs @ {
+    self,
+    flake-parts,
+    nixpkgs,
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = nixpkgs.lib.systems.flakeExposed;
+
+      imports = [
+        ({inputs, ...}: {
+          perSystem = {system, ...}: {
+            _module.args = {
+              pkgs = import inputs.nixpkgs {
+                inherit system;
+              };
+            };
+          };
+        })
+      ];
+
+      perSystem = {
+        pkgs,
+        system,
+        ...
+      }: {
+        packages = import ./default.nix {inherit pkgs;};
+        formatter = pkgs.alejandra;
+      };
     };
 }
